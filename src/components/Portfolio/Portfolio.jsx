26e445fc1c84df0433df16f0d21e0105225e6234
McCreary <Brennan.McCreary@philips.com>
2023-01-30 07:28:27 -0500

    Link portfolio component to Home page.

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,2 +1,16 @@
 //Display studio portfolio here, then link to home.
 //Possibly gallery cards and a game bio.  Maybe on a carousel to save space.
+
+import React from 'react';
+import './Portfolio.css';
+
+const Portfolio = (props) => {
+    return (
+        <div>
+        Placeholder
+        Insert portfolio carousel here
+        </div>
+    )
+}
+
+export default Portfolio;
