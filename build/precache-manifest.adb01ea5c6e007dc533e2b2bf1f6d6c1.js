5e5e29343225f2599ebe9de71c0d9106311dd377
Brennan McCreary <brennanmccreary@gmail.com>
2023-06-14 18:52:43 -0400

    Update social media links.

---
+++ build/precache-manifest.adb01ea5c6e007dc533e2b2bf1f6d6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2956effe56c25b515cd",
+    "url": "/static/css/main.165ebcaf.chunk.css"
+  },
+  {
+    "revision": "f2956effe56c25b515cd",
+    "url": "/static/js/main.f2956eff.chunk.js"
+  },
+  {
+    "revision": "35f9f452859101e2c9f0",
+    "url": "/static/js/1.35f9f452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "040bf7182462b2b0db7a6d9d1b7de836",
+    "url": "/static/media/WHG-Logo-NoBG.040bf718.png"
+  },
+  {
+    "revision": "8f0f152c728a252e0a54eef77cfffed3",
+    "url": "/index.html"
+  }
+];
