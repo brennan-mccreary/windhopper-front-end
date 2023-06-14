326ded41d4b67164955c6aaf7f1c145e6b36504d
Brennan McCreary <brennanmccreary@gmail.com>
2023-06-14 18:38:29 -0400

    Implement SEO measures, logo, and styling.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 //Imports
 import React from 'react';
-import { 
-    Link,
-    Route,
-    Routes } from 'react-router-dom';
+// import { 
+//     Link,
+//     Route,
+//     Routes } from 'react-router-dom';
 import './Home.css';
+import HeaderImage from '../../images/WHG-Logo-NoBG.png';
 
 //Import Components
 import Mission from '../../components/Mission/Mission';
@@ -16,6 +17,7 @@ import Portfolio from '../../components/Portfolio/Portfolio';
 const Home = (props) => {
     return ( 
         <div className='home-container home-bg'>
+            <img src={HeaderImage} alt="Windhopper games logo" className='home-image'/>
             <h1 className='home-title home-center'>Windhopper Games</h1>
             <SocialMediaButtons/>
             <Mission/>
