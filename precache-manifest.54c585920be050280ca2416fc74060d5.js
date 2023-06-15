1393e2c62ea066c34ff1f243944425cb6dfe3927
Brennan McCreary <brennanmccreary@gmail.com>
2023-06-14 20:57:50 -0400

    Updates

---
+++ precache-manifest.54c585920be050280ca2416fc74060d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc28f4c2f3c93b41a16",
+    "url": "/static/css/main.165ebcaf.chunk.css"
+  },
+  {
+    "revision": "5cc28f4c2f3c93b41a16",
+    "url": "/static/js/main.5cc28f4c.chunk.js"
+  },
+  {
+    "revision": "35f9f452859101e2c9f0",
+    "url": "/static/js/1.35f9f452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64bbf31157756468a6bf8f181c3bf9ba",
+    "url": "/static/media/WHG_Logo.64bbf311.svg"
+  },
+  {
+    "revision": "c82053c36950982a831a50cf28798fc1",
+    "url": "/index.html"
+  }
+];
